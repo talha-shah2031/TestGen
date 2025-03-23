697f48e59f5ad8026a7b43465f07b0e0349481df
Syed Talha Shah <167776068+talha-shah2031@users.noreply.github.com>
2025-03-23 22:14:23 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import TestGenerator from './pages/TestGenerator';
+import StudyRoom from './pages/StudyRoom'; // Assuming StudyRoom is in components folder
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-purple-50 to-pink-50 flex flex-col">
@@ -14,12 +15,13 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/generator" element={<TestGenerator />} />
+            <Route path="/study-room" element={<StudyRoom />} />
           </Routes>
         </div>
         <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
